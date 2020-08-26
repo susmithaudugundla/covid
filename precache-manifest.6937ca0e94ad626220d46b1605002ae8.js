296673ae7f8c332b01c1f64f25b84498dd72e19e
Susmitha <udugundlasusmitha@gmail.com>
2020-08-26 20:08:13 +0530

    Updates

---
+++ precache-manifest.6937ca0e94ad626220d46b1605002ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495d7bf8f98554924986d25918428c4d",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "55d62bb3ae6b4d3b8494",
+    "url": "/covid/static/css/main.a179677d.chunk.css"
+  },
+  {
+    "revision": "18712c6cba18c33c8815",
+    "url": "/covid/static/js/2.42f1570b.chunk.js"
+  },
+  {
+    "revision": "a8fa0a685d8af2934ba8ade7768fed07",
+    "url": "/covid/static/js/2.42f1570b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d62bb3ae6b4d3b8494",
+    "url": "/covid/static/js/main.065100a7.chunk.js"
+  },
+  {
+    "revision": "89e549afabfa843f7afd",
+    "url": "/covid/static/js/runtime-main.1bec297b.js"
+  },
+  {
+    "revision": "77eb48c3646688efa70f953357b1cecf",
+    "url": "/covid/static/media/index.77eb48c3.svg"
+  }
+]);
